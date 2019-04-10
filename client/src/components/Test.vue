f4ad3808c4601430ff67738d0393bc2d33297522
vincentlunc <xiaopeng@live.unc.edu>
2019-04-10 17:29:52 -0400

    modify backend; add str to mdl feature

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -183,154 +183,142 @@
       this.stopRecording()
     },
 
-      onSubmit (evt) {
-        evt.preventDefault();
+    onSubmit (evt) {
+      evt.preventDefault();
+      
+      const query = {query:this.form.speechInp};
+      console.log(query)
+      this.fetchResult(query);
+    },
+
+    filter (_resObj) {
+      if (this.isResult&this.noError){
+        return Object.values(_resObj).slice((this.currentPage-1)*10, (this.currentPage-1)*10+10);
         
-        const query = {query:this.form.speechInp};
-        console.log(query)
-        this.fetchResult(query);
-      },
-
-      filter (_resObj) {
-        if (this.isResult&this.noError){
-          return Object.values(_resObj).slice((this.currentPage-1)*10, (this.currentPage-1)*10+10);
-          
-        }
-      },
-      successCallback(stream) {
-        const vm = this;
-        console.log('successCallback:...IN');
-        console.log('successCallback:...IN');
-        // add user gesture
-        console.log(audioContext.resume());
-        var input = audioContext.createMediaStreamSource(stream);
-        //console.log(input)
-        var bufferSize = 2048;
-        scriptNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
-        scriptNode.onaudioprocess = scriptNodeProcess;
-        input.connect(scriptNode);
-  
-        // console.log('ScriptNode BufferSize:', scriptNode.bufferSize);
-        function scriptNodeProcess(audioProcessingEvent) {
-        var inputBuffer = audioProcessingEvent.inputBuffer;
-        var outputBuffer = audioProcessingEvent.outputBuffer;
-        var inputData = inputBuffer.getChannelData(0);
-        var outputData = outputBuffer.getChannelData(0);
-
-  
-        // Loop through the 4096 samples
-        for (var sample = 0; sample < inputBuffer.length; sample++) {
-          outputData[sample] = inputData[sample];
-        }
-        ssStream.write(new ss.Buffer(vm.downsampleBuffer(inputData, 44100, 16000)));
       }
-      },
-      downsampleBuffer(buffer, sampleRate, outSampleRate) {
-        if (outSampleRate == sampleRate) {
-          return buffer;
-        }
-        if (outSampleRate > sampleRate) {
-          throw "downsampling rate show be smaller than original sample rate";
-        }
-        var sampleRateRatio = sampleRate / outSampleRate;
-        var newLength = Math.round(buffer.length / sampleRateRatio);
-        var result = new Int16Array(newLength);
-        var offsetResult = 0;
-        var offsetBuffer = 0;
-        while (offsetResult < result.length) {
-          var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-          var accum = 0,
-            count = 0;
-          for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-            accum += buffer[i];
-            count++;
-          }
-  
-          result[offsetResult] = Math.min(1, accum / count) * 0x7FFF;
-          offsetResult++;
-          offsetBuffer = nextOffsetBuffer;
-        }
-        return result.buffer;
-      },
-      startRecording() {
+    },
+    successCallback(stream) {
+      const vm = this;
+      console.log('successCallback:...IN');
+      console.log('successCallback:...IN');
+      // add user gesture
+      console.log(audioContext.resume());
+      var input = audioContext.createMediaStreamSource(stream);
+      //console.log(input)
+      var bufferSize = 2048;
+      scriptNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+      scriptNode.onaudioprocess = scriptNodeProcess;
+      input.connect(scriptNode);
 
-        console.log("recording!!");
-        // as
-        // const languageSelected = this.selected;
-        // socket.emit('LANGUAGE_SPEECH', languageSelected);
-        this.result = true;
-        this.btn = false;
-        this.btnStop = true;
-        this.btnReset = false;
-        scriptNode.connect(audioContext.destination);
-        console.log(audioContext.destination);
-        console.log("START_SPEECH"); 
-        ss(socket).emit('START_SPEECH', ssStream);
-        setInterval(function() {
-          this.stopRecording();
-        }.bind(this), 55000);
-      },
-      stopRecording() {
-	      console.log(this.form.speechInp)
-        console.log("Stop recording!");
-        this.btnStop = false;
-        this.btn = true;
-        this.btnReset = true;
-        console.log(audioContext.destination);
-        scriptNode.disconnect(audioContext.destination);
-        ssStream.end();
-        socket.emit('STOP_SPEECH', {});
-  console.log(this.form.speechInp)
-	// send to flask here for speech to result (regex) way
-  const query = {'query':this.form.speechInp}
-  console.log(query)
-	const path = 'http://3.86.172.253:5001/speech_regex';
-        axios.post(path, query)
-          .then((res)=>{
-            console.log(res);
-	          this.form.speechInp = res.data.res_query;
-           })
-          .catch((error) => {
-            console.error(error);
-           });
+      // console.log('ScriptNode BufferSize:', scriptNode.bufferSize);
+      function scriptNodeProcess(audioProcessingEvent) {
+      var inputBuffer = audioProcessingEvent.inputBuffer;
+      var outputBuffer = audioProcessingEvent.outputBuffer;
+      var inputData = inputBuffer.getChannelData(0);
+      var outputData = outputBuffer.getChannelData(0);
 
-      },
-      errorCallback(error) {
-        // console.log('errorCallback:', error);
-      },
-      redirectError(){
-        this.noError = true;
-        // refresh page
-	this.$router.go(0);
-      },
+
+      // Loop through the 4096 samples
+      for (var sample = 0; sample < inputBuffer.length; sample++) {
+        outputData[sample] = inputData[sample];
+      }
+      ssStream.write(new ss.Buffer(vm.downsampleBuffer(inputData, 44100, 16000)));
+    }
+    },
+    downsampleBuffer(buffer, sampleRate, outSampleRate) {
+      if (outSampleRate == sampleRate) {
+        return buffer;
+      }
+      if (outSampleRate > sampleRate) {
+        throw "downsampling rate show be smaller than original sample rate";
+      }
+      var sampleRateRatio = sampleRate / outSampleRate;
+      var newLength = Math.round(buffer.length / sampleRateRatio);
+      var result = new Int16Array(newLength);
+      var offsetResult = 0;
+      var offsetBuffer = 0;
+      while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+        var accum = 0,
+          count = 0;
+        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+          accum += buffer[i];
+          count++;
+        }
+
+        result[offsetResult] = Math.min(1, accum / count) * 0x7FFF;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+      }
+      return result.buffer;
+    },
+    startRecording() {
+
+      console.log("recording!!");
+      // as
+      // const languageSelected = this.selected;
+      // socket.emit('LANGUAGE_SPEECH', languageSelected);
+      this.result = true;
+      this.btn = false;
+      this.btnStop = true;
+      this.btnReset = false;
+      scriptNode.connect(audioContext.destination);
+      console.log(audioContext.destination);
+      console.log("START_SPEECH"); 
+      ss(socket).emit('START_SPEECH', ssStream);
+      setInterval(function() {
+        this.stopRecording();
+      }.bind(this), 55000);
+    },
+    stopRecording() {
+      console.log(this.form.speechInp)
+      console.log("Stop recording!");
+      this.btnStop = false;
+      this.btn = true;
+      this.btnReset = true;
+      console.log(audioContext.destination);
+      scriptNode.disconnect(audioContext.destination);
+      ssStream.end();
+      socket.emit('STOP_SPEECH', {});
+      console.log(this.form.speechInp)
+      // send to flask here for speech to result (regex) way
+      const query = {'query':this.form.speechInp}
+      console.log(query)
+      // const path = 'http://3.86.172.253:5001/speech_regex';
+      const path = 'http://localhost:5001/speech_regex';
+      axios.post(path, query)
+        .then((res)=>{
+          console.log(res);
+          this.form.speechInp = res.data.res_query;
+          })
+        .catch((error) => {
+          console.error(error);
+          });
+    },
+    errorCallback(error) {
+      // console.log('errorCallback:', error);
+    },
+    redirectError(){
+      this.noError = true;
+      // refresh page
+      this.$router.go(0);
+    },
     },
 
     created() {
        const that = this;
-       // console.log(audioContext) 
-       // console.log("created")
-       // console.log(that.result, that.btn, that.btnStop);
        socket.on('SPEECH_RESULTS', function(text) {
-         // console.log(text)
          if('q' == text){
-
            that.resultError = true;
            console.log("error")
          }else{
-           // console.log(text)
-        
-           // that.textResult = text;
            that.form.name = text;
          }
        })
          if (navigator.mediaDevices.getUserMedia) {
-           // console.log('getUserMedia supported...');
            navigator.webkitGetUserMedia({ audio: true }, function(stream) {
-             // console.log("stream?");
-             // // console.log(stream);
              that.successCallback(stream)
            }, function(error) {
-             // console.log("error2")
              that.errorCallback(error)
            });
          } else {
