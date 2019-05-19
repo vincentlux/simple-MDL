093e20eaacb614dcca5b725bcf01faf22e012eca
vincentlunc <xiaopeng@live.unc.edu>
2019-05-18 23:12:39 -0400

    finish xml conversion

--- solr/indexing.py
+++ solr/indexing.py
@@ -1,7 +1,7 @@
 from mbox2solr import mbox2solr
 
 if __name__ == '__main__':
-    m2s = mbox2solr(f_name='chunk_1.txt', dir_in='./data/Mail', dir_out='./data/txt')
+    m2s = mbox2solr(f_name='chunk_0_filtered.txt', dir_in='./data/Mail', dir_out='./data/xml')
     print('start')
     m2s.split(remove_trashcode=True, save_as_txt=False)
     m2s.txt2xml()
--- solr/mbox2solr.py
+++ solr/mbox2solr.py
@@ -15,6 +15,7 @@ class mbox2solr:
         self.create_dir(self.dir_in)
         self.create_dir(self.dir_out)
         self.res_docs = [] # save all docs in a list
+        self.debug = []
 
         self.start           = '<add>\n<doc>\n'
         self.f_message_id    = '<field name="message_id">'
@@ -22,11 +23,11 @@ class mbox2solr:
         self.f_from          = '<field name="from">'
         self.f_to            = '<field name="to">'
         self.f_subject       = '<field name="subject">'
-        # self.f_from_name     = '<field name="from_name">'
-        # self.f_to_name       = '<field name="to_name">'
         self.f_content       = '<field name="content">'
         self.close           = '</field>\n'
         self.end             = '</doc>\n</add>'
+        # self.f_from_name     = '<field name="from_name">'
+        # self.f_to_name       = '<field name="to_name">'
 
     def split(self, remove_trashcode=False, save_as_txt=False):
         """convert mbox to separate txt files ready for xml indexing 
@@ -47,22 +48,32 @@ class mbox2solr:
                     # only get Content-Type: text/plain
                     if remove_trashcode:
                         # get content between two Cconten-Type
+                        if line.startswith('Content-Type: multipart'):
+                            multi = True
                         if line.startswith('Content-Type: text/plain'): 
                             temp_doc += line
                             line = f.readline()
-                            while not line.startswith('Content-Type:'):
-                                temp_doc += line
-                                line = f.readline()
-                            # this doc ends, now to consume all trash from this doc
-                            while not line.startswith('From '):
-                                line = f.readline()
-                                if not line:
-                                    break
+                            if multi:
+                                while not line.startswith('Content-Type:'): #  and not(line.startswith('From ') and line[])
+                                    temp_doc += line
+                                    line = f.readline()
+                                # this doc ends, now to consume all trash from this doc
+                                while not line.startswith('From '):
+                                    line = f.readline()
+                                    if not line: # last one
+                                        break
+                            else:
+                                while not line.startswith('From'): #  and not(line.startswith('From ') and line[])
+                                    temp_doc += line
+                                    line = f.readline()
+                                    if not line: # last one
+                                        break
                     # if EOF, end
                     if not line:
                         end = True
                         break
 
+                multi = False
                 if save_as_txt:
                     with open(os.path.join(self.dir_out, str(count)+'.txt'), 'w') as ff:
                         ff.write(temp_doc)
@@ -82,12 +93,13 @@ class mbox2solr:
     def txt2xml(self):
         """try to convert a string to xml string directly 
         (instead of save .txt+.xml)
-        idea: 先split by \n 再在最后加\n 合起来成xml string
+        idea: first split by \n then concat to a xml string
         """
         if not self.res_docs:
             print('does not have txt saved as var')
             raise NameError
         print(len(self.res_docs))
+        count = 0
         for doc in self.res_docs:
             temp_dict = {}
             date = False
@@ -97,52 +109,100 @@ class mbox2solr:
             # start processing each file
             for line in lines[1:]:
                 if not line.startswith('Content-Type: text/plain') and not content:
-                    if ":" in line:
-                        if line.startswith("Date:") and not date:
+                    if ':' in line:
+                        if line.startswith('Date:') and not date:
                             try:
                                 date = True
-                                split_line = line.strip().split(":", 1)
+                                split_line = line.strip().split(':', 1)
                                 # strip time zone and format time
                                 split_line[1] = split_line[1].strip()
-                                split_line[1] = datetime.strptime(split_line[1], "%a, %d %b %Y %H:%M:%S %z").strftime('%Y-%m-%dT%H:%M:%SZ')
+                                split_line[1] = datetime.strptime(split_line[1], '%a, %d %b %Y %H:%M:%S %z').strftime('%Y-%m-%dT%H:%M:%SZ')
+                                
+                            except ValueError:
+                                try: # may contain (UTC) at the end
+                                    split_line[1] = datetime.strptime(split_line[1][:-6].strip(), '%a, %d %b %Y %H:%M:%S %z').strftime('%Y-%m-%dT%H:%M:%SZ')
+                                except ValueError:
+                                    try: # may be Sat, 28 May 2016 12:25:12 EDT
+                                        split_line[1] = datetime.strptime(split_line[1][:-4].strip(), '%a, %d %b %Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
+                                    except ValueError: # may be '3 Apr 2015 08:42:29 -0400'
+                                        split_line[1] = datetime.strptime(split_line[1], '%d %b %Y %H:%M:%S %z').strftime('%Y-%m-%dT%H:%M:%SZ')
+                                except Exception as e:
+                                    print('error time!',e)
+                                    print(line)
+                                    break
+
+                            finally:
                                 split_line[1] = self.xmlescape(split_line[1].strip())
-                                print(split_line)
-                            except:
-                                print('error time!')
-                                print(line, lines)
-                                break
+                                    
                         else:
-                            if line.startswith("Date:"): # add to the former attribute
-                                split_line[1] += self.xmlescape(line.strip())
-                                print('potential error')
-                                print(split_line[1])
-                            else:
-                                split_line = line.strip().split(":", 1)
-                                split_line[1] = self.xmlescape(split_line[1].strip())
+                            # if line.startswith("Date:"): # this is probably not possible because not yet to the content
+                            #     split_line[1] += self.xmlescape(line.strip())
+                            split_line = line.strip().split(":", 1)
+                            split_line[1] = self.xmlescape(split_line[1].strip())
                     else:
                         split_line[1] += self.xmlescape(re.sub(r"[\s+]", ' ', line))
                     temp_dict[split_line[0]] = split_line[1]
                 else:
-                    # content
-                    # to remove useless content lines
-                    # if line.startswith('Content-Type:') or line.startswith('Content-Transfer'):
-                    #     content = True
-                    #     continue
                     content = True
                     try:
-                        temp_dict["content"] += self.xmlescape(line)
+                        # catch possible useful items under content-type (exception)
+                        if (line.startswith('Message-Id') and 'Message-Id' not in temp_dict.keys()) \
+                            or (line.startswith('Message-ID') and 'Message-ID' not in temp_dict.keys()) \
+                            or (line.startswith('Message-id') and 'Message-id' not in temp_dict.keys()) \
+                            or (line.startswith('Date') and 'Date' not in temp_dict.keys()) \
+                            or (line.startswith('Subject') and 'Subject' not in temp_dict.keys()) \
+                            or (line.startswith('From') and 'From' not in temp_dict.keys()) \
+                            or (line.startswith('To') and 'To' not in temp_dict.keys()):
+                            split_line = line.strip().split(":", 1)
+                            split_line[1] = self.xmlescape(split_line[1].strip())
+                            temp_dict[split_line[0]] = split_line[1]
+
+                        else:
+                            temp_dict['content'] += self.xmlescape(line)
                     except:
-                        temp_dict["content"] = ""
-                        temp_dict["content"] += self.xmlescape(line)
+                        temp_dict['content'] = ''
+                        temp_dict['content'] += self.xmlescape(line)
 
-            
-            for k,v in temp_dict.items():
-                print(k,v)
-            raise NotImplementedError
             # convert to xml
+            xml = ''
+            try:
+                try:
+                    _message_id = self.f_message_id + temp_dict['Message-ID'] + self.close
+                except KeyError:
+                    try:
+                        _message_id = self.f_message_id + temp_dict['Message-Id'] + self.close
+                    except KeyError:
+                        _message_id = self.f_message_id + temp_dict['Message-id'] + self.close
+                except:
+                    print(temp_dict.keys())
 
-            
+                _date = self.f_date + temp_dict['Date'] + self.close
+                _from = self.f_from + temp_dict['From'] + self.close
+                try: 
+                    _to = self.f_to + temp_dict['To'] + self.close
+                except KeyError:
+                    temp_dict['To'] = ''
+                    _to = self.f_to + temp_dict['To'] + self.close
+                try:
+                    _subject = self.f_subject + temp_dict['Subject'] + self.close
+                except:
+                    temp_dict['Subject'] = ''
+                    _subject = self.f_subject + temp_dict['Subject'] + self.close
+                try:
+                    _content = self.f_content + temp_dict['content'] + self.close
+                except:
+                    temp_dict['content'] = ''
+                    _content = self.f_content + temp_dict['content'] + self.close
+                xml = self.start + _message_id + _date + _from + _to + _subject + _content + self.end
+            except Exception as e:
+                # print(count,temp_dict)
+                self.debug.append((e,count))
+            with open(os.path.join(self.dir_out, str(count)+'.xml'), 'w') as ff:
+                ff.write(xml)
+            count += 1
+        print(self.debug)
 
+    
 
     def create_dir(self, dir_in):
         if not os.path.exists(dir_in):
@@ -155,4 +215,3 @@ class mbox2solr:
         })
     
        
-
